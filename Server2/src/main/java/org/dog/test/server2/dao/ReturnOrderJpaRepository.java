0bcd6b2cf071f6369f50d742fdcbc3975545e845
Peng <sunpeng@qianmi.com>
2019-04-10 14:16:13 +0800

    修改demo

--- Server2/src/main/java/org/dog/test/server2/dao/ReturnOrderJpaRepository.java
+++
@@ -1,6 +0,0 @@
-package top.dogtcc.test.server2.dao;
-
-import top.dogtcc.database.spring.jpa.DogJpaRepository;
-
-public interface ReturnOrderJpaRepository extends DogJpaRepository<ReturnOrder, String> {
-}
